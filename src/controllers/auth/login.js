ca8544cb3374d6f7c038bda82c8fe06d09ef2260
satyaaa111 <juman60000@gmail.com>
2024-01-24 12:59:50 +0530

    created dummy login logic integrating with database

--- src/controllers/auth/login.js
+++ src/controllers/auth/login.js
@@ -1,50 +1,37 @@
-const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-
-
-// Replace this with your actual user database
-const users = [
-  {
-    id: 1,
-    username: 'exampleuser',
-    // Replace 'password123' with the hashed password for 'password123'
-    password: '$2b$10$jNSaTBNgOJ2HmjWD6rMg/uvcVRcmgr15cHs1ZM3RsRZjURj.nxBnG',
-  },
-];
+const User = require('../../models/users.js');
 
 const login = async (req, res) => {
-    
-    const { username, password } = req.body;
-    
+
+  const { name, password } = req.body;
+
+  try {
     // Find the user by username
-    const user = users.find((user) => user.username === username);
-  
+    const user = await User.findOne({ name });
+
     if (!user) {
       return res.status(401).json({ message: 'Invalid username or password' });
     }
- 
-    try {
-      // Compare the provided password with the hashed password in the database
-      const passwordMatch = await bcrypt.compare(password, user.password);
-  
-      if (!passwordMatch) {
-        return res.status(401).json({ message: 'Invalid username or password' });
-      }
-  
-      // If the password is correct, create a JWT token
-      const token = jwt.sign({ userId: user.id, username: user.username }, 'your-secret-key', {
-        expiresIn: '1h', // Token expiration time
-      });
-  
-      res.json({ token });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
+
+    // Compare the provided password with the hashed password in the database
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Invalid username or password' });
     }
+
+    // If the password is correct, create a JWT token
+    const token = jwt.sign({ userId: user.password, username: user.name }, process.env.SECRET_KEY);
+
+    // console.log(token);
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
   }
-  
-module.exports = {
-    login,
 };
 
+module.exports = {
+  login,
+};
